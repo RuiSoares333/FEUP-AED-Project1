6d15cbfe6eca4ca216b6d32e440245a27f67d36c
Valkajjik <lcsferreiradesousa@hotmail.com>
2021-11-27 17:59:37 +0000

    project started

---
+++ cmake-build-debug/CMakeFiles/projeto_aed.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projeto_aed.dir/main.cpp.obj"
+  "libprojeto_aed.dll.a"
+  "projeto_aed.exe"
+  "projeto_aed.exe.manifest"
+  "projeto_aed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projeto_aed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
