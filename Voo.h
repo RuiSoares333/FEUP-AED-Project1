5fc3ee4fd31a9a1eb80adab8be10de991b6e6d4f
RuiSoares333 <93407914+RuiSoares333@users.noreply.github.com>
2021-12-19 18:13:03 +0000

    Update Voo.h

--- Voo.h
+++ Voo.h
@@ -29,6 +29,7 @@ public:
     void setData(Date data);
     void addPassageiro(Passageiro& passageiro);
     bool removePassageiro(Passageiro& passageiro); //remove passageiro do voo, retorna true se encontrou o passageiro senao false
+    void addListaPassageiro(list<Passageiro> passageiros);
     bool saveFile ();
     bool loadFile ();
     bool const operator < (Voo v2) const;
