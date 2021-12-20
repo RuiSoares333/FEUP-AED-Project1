2824fc003f96acef5c7de22924e7d4b1bda94d5e
RuiSoares333 <93407914+RuiSoares333@users.noreply.github.com>
2021-12-20 18:56:32 +0000

    Update Bagagem.cpp

--- Bagagem.cpp
+++ Bagagem.cpp
@@ -4,6 +4,8 @@
 
 #include "Bagagem.h"
 
+/// Constructor
+/// Construtor Vazio de Bagagem com Valores Default
 Bagagem::Bagagem() {
     this->comprimento = 0;
     this->largura = 0;
@@ -11,6 +13,8 @@ Bagagem::Bagagem() {
     this->peso = 0;
 }
 
+/// Constructor
+/// Construtor completo com \param comprimento, \param largura, \param altura e \param peso
 Bagagem::Bagagem(float comprimento, float largura, float altura, float peso) {
     this->comprimento =  comprimento;
     this->largura = largura;
@@ -18,18 +22,26 @@ Bagagem::Bagagem(float comprimento, float largura, float altura, float peso) {
     this->peso = peso;
 }
 
+/// Getter
+/// \return \param comprimento
 float Bagagem::getComprimento() const {
     return comprimento;
 }
 
+/// Getter
+/// \return \param largura
 float Bagagem::getLargura() const {
     return largura;
 }
 
+/// Getter
+/// \return \param altura
 float Bagagem::getAltura() const {
     return altura;
 }
 
+/// Getter
+/// \return \param peso
 float Bagagem::getPeso() const {
     return peso;
 }
