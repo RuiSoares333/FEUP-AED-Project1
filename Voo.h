85f150861dbe7dee1465e25517efb04a1f2c3b19
RuiSoares333 <93407914+RuiSoares333@users.noreply.github.com>
2021-12-20 19:10:50 +0000

    Update Voo.h

--- Voo.h
+++ Voo.h
@@ -17,16 +17,20 @@ using namespace std;
 class Voo {
 public:
     Voo();
-    Voo(int numVoo, Date dataPartida, float duracaoVoo, list<Passageiro> passageiros, TransporteBagagem transporteBagagem);
+    Voo(int numVoo, Date dataPartida, float duracaoVoo, list<Passageiro> passageiros, TransporteBagagem transporteBagagem, Aeroporto origem, Aeroporto destino);
     int getNum() const;
     Date getData() const ;
     float getDuracao() const;
     list<Passageiro> getPassageiros() const;
+    Aeroporto getOrigem() const;
+    Aeroporto getDestino() const;
     void setPassageiros(list<Passageiro> passageiros);
     void setTB(TransporteBagagem t);
     void setNum(int num);
     void setDuracao(int dur);
     void setData(Date data);
+    void setOrigem(const Aeroporto &origem);
+    void setDestino(const Aeroporto &destino);
     void addPassageiro(Passageiro& passageiro);
     bool removePassageiro(Passageiro& passageiro); //remove passageiro do voo, retorna true se encontrou o passageiro senao false
     void addListaPassageiro(list<Passageiro> passageiros);
@@ -40,6 +44,8 @@ private:
     float duracaoVoo;
     list<Passageiro> passageiros;
     TransporteBagagem transporteBagagem;
+    Aeroporto origem;
+    Aeroporto destino;
 };
 
 
